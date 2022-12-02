d174b9c93362f3c40c7e128260a6c09b825c88da
Sam Estep <sam@samestep.com>
2022-12-02 17:28:33 -0500

    Try pulling before building Docker image

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -ex
 poetry export --without-hashes --output=requirements.txt
 wget -nc https://www.johnvansickle.com/ffmpeg/releases/ffmpeg-5.1.1-amd64-static.tar.xz
-docker build --platform=linux/amd64 --tag=whisper:latest .
+docker build --platform=linux/amd64 --pull --cache-from=whisper:latest --tag=whisper:latest .
